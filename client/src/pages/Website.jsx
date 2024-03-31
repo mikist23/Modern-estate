031a57222a88b350c873d1d399f03aaeff3259a0
@Mikist <mikeochango3@gmail.com>
2024-03-31 14:48:31 +0300

    Created diffetent pages for the front end part

--- client/src/pages/Website.jsx
+++ client/src/pages/Website.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import "../App.css"; 
 import Companies from "../components/Companies/Companies";
 import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
@@ -9,21 +8,20 @@ import Hero from "../components/Hero/Hero";
 import Residencies from "../components/Residencies/Residencies";
 import Value from "../components/Value/Value";
 
+
 const Website = () => {
   return (
-     <div className="App">
-      <div>
-        <div className="white-gradient" />
-        <Header />
-        <Hero />
-      </div>
-      <Companies />
-      <Residencies/>
-      <Value/>
-      <Contact/>
-      <GetStarted/>
-      <Footer/>
+    <div className="App">
+    <div>
+      <div className="white-gradient" />
+      <Hero />
     </div>
+    <Companies />
+    <Residencies/>
+    <Value/>
+    <Contact/>
+    <GetStarted/>
+  </div>
   )
 }
 
