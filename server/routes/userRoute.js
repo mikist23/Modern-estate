63629aae96547d1f6ef5cca190110a0a18c365e9
@Mikist <mikeochango3@gmail.com>
2024-03-31 14:39:48 +0300

    adedd jwt check for routes

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,10 +4,10 @@ import { createUser, bookVisit, getAllBookings, cancelBooking, toFav, getAllFavo
 
 const router = express.Router();
 
-router.post("/register", createUser);
-router.post("/bookVisit/:id", bookVisit);
+router.post("/register", jwtCheck, createUser);
+router.post("/bookVisit/:id", jwtCheck, bookVisit);
 router.post("/allBookings", getAllBookings);
-router.post("/removeBooking/:id", cancelBooking);
-router.post("/toFav/:rid", toFav);
-router.post("/allFav/",  getAllFavorites);
+router.post("/removeBooking/:id", jwtCheck, cancelBooking);
+router.post("/toFav/:rid", jwtCheck, toFav);
+router.post("/allFav/", jwtCheck, getAllFavorites);
 export { router as userRoute };
